3877416b42ab83c534e8e969173220f2e7e8d5a1
Devin Xie <devinxie418@gmail.com>
2026-04-22 13:02:23 -0400

    seeding script

---
+++ prisma/seed.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../config/prismaClient.js";
+
+async function main() {
+    const username = process.env.AUTHOR_USERNAME;
+    const password = process.env.AUTHOR_PASSWORD;
+
+    const existing = await prisma.user.findUnique({
+        where: { username }
+    });
+
+    if (existing) {
+        console.log("Admin already exists");
+        return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword,
+            role: "AUTHOR",
+            provider: "LOCAL"
+        }
+    });
+
+    console.log("Admin created");
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
