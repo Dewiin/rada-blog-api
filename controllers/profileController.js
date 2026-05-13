465dc14e311166d93997ec39fb22b43fa0fe9f7a
Devin Xie <devinxie418@gmail.com>
2026-05-12 20:34:54 -0400

    feat: implement getActivity

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -28,7 +28,7 @@ async function getAllPublishedPosts(req, res) {
     } catch (err) {
         console.error("Error in getAllPublishedPosts:", err.message, err.stack);
         return res.status(500).json({
-            error: "Database query failed for getAllPublishedPosts.",
+            error: "Server error getting published posts.",
         });
     } 
 }
@@ -61,12 +61,78 @@ async function getAllUnpublishedPosts(req, res) {
     } catch (err) {
         console.error("Error in getAllUnpublishedPosts:", err.message, err.stack);
         return res.status(500).json({
-            error: "Database query failed for getAllUnpublishedPosts.",
+            error: "Server error getting unpublished posts.",
         });
     } 
 }
 
+async function getActivity(req, res) {
+    try {
+        const clapsActivity = (await prisma.post.findMany({
+            where: {
+                claps: {
+                    some: {
+                        userId: req.user.id
+                    }
+                }
+            },
+            include: {
+                author: true,
+                comments: {
+                    include: {
+                        user: true,
+                    }
+                },
+                claps: true,
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })).map((activity) => ({
+            ...activity,
+            type: "clap"
+        }));
+
+        const commentsActivity = (await prisma.post.findMany({
+            where: {
+                comments: {
+                    some: {
+                        userId: req.user.id,
+                    }
+                }
+            },
+            include: {
+                author: true,
+                comments: {
+                    include: {
+                        user: true,
+                    }
+                },
+                claps: true,
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })).map((activity) => ({
+            ...activity,
+            type: "comment"
+        }));
+
+        return res.status(200).json({ 
+            clapsActivity, 
+            commentsActivity, 
+            message: "Activity fetched successfully!" 
+        });
+    } catch (err) {
+        console.error("Error in getActivity:", err.message, err.stack);
+        return res.status(500).json({
+            error: "Server error in getting user activity.",
+        });
+    }
+}
+
 export const profileController = {
     getAllPublishedPosts,
-    getAllUnpublishedPosts
+    getAllUnpublishedPosts,
+    getActivity
 }
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { profileController } from "../controllers/profileController.js";
 import { verifyAuth } from "../middleware/verifyAuth.js";
+import { verifyToken } from "../middleware/verifyToken.js"
 export const profileRouter = Router();
 
 profileRouter.get("/published", verifyAuth, profileController.getAllPublishedPosts);
 profileRouter.get("/unpublished", verifyAuth, profileController.getAllUnpublishedPosts);
+profileRouter.get("/activity", verifyToken, profileController.getActivity);
